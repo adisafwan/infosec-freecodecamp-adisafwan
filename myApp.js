d77a2cb73022a3f888f6ba31463bdc026bbd5a40
Adi Safwan <adisafwan20082001@gmail.com>
2023-10-05 15:56:51 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // my code
 app.listen(PORT, () => {
